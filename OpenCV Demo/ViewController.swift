3ec035d2d56a0edbe5a2dfe00f5d1e57f229add5
Mohammad Shaker <mohamed.shaker@itworx.com>
2020-03-19 19:13:02 +0200

    Initial Commit

---
+++ OpenCV Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenCV Demo
+//
+//  Created by Shaker on 3/19/20.
+//  Copyright © 2020 Shaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
